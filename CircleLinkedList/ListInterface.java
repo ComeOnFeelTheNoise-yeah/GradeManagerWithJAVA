4683440c6049a96885f7a6fec97785d03b899bd7
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-07-22 03:24:27 +0900

    Create ListInterface.java

---
+++ CircleLinkedList/ListInterface.java
@@ -0,0 +1,1 @@
+package CircleLinkedList;public interface ListInterface<E> {	public void add(int index, E x);	public void append(E x);	public E remove(int index);	public boolean removeStudent(E x);	public E get(int index);	public void set(int index, E x);	public int indexOf(int index, E x);	public int size();	public boolean isEmpty();	public void clear();}
