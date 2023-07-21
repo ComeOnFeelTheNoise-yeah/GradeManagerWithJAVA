f6f134f7a9e696d988cac07db52e83bf6e1287a0
Seung Ju Han <nick.washswat@gmail.com>
2023-07-22 03:31:31 +0900

    Update ListInterface.java

--- CircleLinkedList/ListInterface.java
+++ CircleLinkedList/ListInterface.java
@@ -1,1 +1,14 @@
-package CircleLinkedList;public interface ListInterface<E> {	public void add(int index, E x);	public void append(E x);	public E remove(int index);	public boolean removeStudent(E x);	public E get(int index);	public void set(int index, E x);	public int indexOf(int index, E x);	public int size();	public boolean isEmpty();	public void clear();}
+package CircleLinkedList;
+
+public interface ListInterface<E> {
+	public void add(int index, E x);
+	public void append(E x);
+	public E remove(int index);
+	public boolean removeStudent(E x);
+	public E get(int index);
+	public void set(int index, E x);
+	public int indexOf(int index, E x);
+	public int size();
+	public boolean isEmpty();
+	public void clear();
+}
